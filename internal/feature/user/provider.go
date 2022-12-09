04bbdcd17f239683667871403ce9782997018e98
Made Indra <made.indra@protonmail.com>
2022-12-09 17:39:14 +0700

    create provider and wire

--- internal/feature/user/provider.go
+++ internal/feature/user/provider.go
@@ -1,1 +1,36 @@
 package user
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+)
+
+func ProvideUserRepository(db *sql.DB) *repository {
+	return &repository{
+		db: db,
+	}
+}
+
+func ProvideUserService(repo UserRepository) *service {
+	return &service{
+		r: repo,
+	}
+}
+
+func ProvideUserHandler(service UserService) *handler {
+	return &handler{
+		s: service,
+	}
+}
+
+var UserProviderSet wire.ProviderSet = wire.NewSet(
+	ProvideUserRepository,
+	ProvideUserService,
+	ProvideUserHandler,
+
+	// bind to the interface because the provider return concrete, not interface as needed by the dependants
+	wire.Bind(new(UserRepository), new(*repository)),
+	wire.Bind(new(UserService), new(*service)),
+	wire.Bind(new(UserHandler), new(*handler)),
+)
--- internal/feature/user/wire.go
+++ internal/feature/user/wire.go
@@ -1,1 +1,15 @@
+//go:build wireinject
+// +build wireinject
+
 package user
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+)
+
+// dependency injection to create repo -> service -> handler
+func InjectUser(db *sql.DB) *handler {
+	panic(wire.Build(UserProviderSet))
+}
